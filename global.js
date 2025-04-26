02d1a783b45217cbaae2d8b794fe787a2f8de42c
Brian Can <canbrian59@gmail.com>
2025-04-25 20:08:46 -0700

    lab4

--- global.js
+++ global.js
@@ -71,5 +71,35 @@ select.addEventListener('input', function (event) {
   localStorage.colorScheme = scheme;
 });
 
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
 
-  
+export function renderProjects(project, containerElement, headingLevel = 'h2') {  
+  containerElement.innerHTML = '';
+
+  project.forEach((project) => {
+    const article = document.createElement('article');
+    article.innerHTML = `
+    <h3>${project.title}</h3>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  });
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
+
